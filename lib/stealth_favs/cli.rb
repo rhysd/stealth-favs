96a185ffe53b3c43ded05de70ccc69c70657af57
rhysd <lin90162@yahoo.co.jp>
2013-05-30 22:42:32 +0900

    implement and improve commands
        - refactored method names for commands
        - add command implementations
        - add --verbose flag

--- lib/stealth_favs/cli.rb
+++ lib/stealth_favs/cli.rb
@@ -10,16 +10,22 @@ module StealthFavs
 
     def self.def_command(name)
       define_method name do |*args|
-        Main::__send__ name,options,*args
+        Main::__send__ "cmd_#{name}",options,*args
       end
+    end
+
+    def self.verbose_option
+      method_option :verbose, :type => :boolean, :aliases => '-v', :default => true, :desc => 'output result'
     end
 
     public
 
     desc 'user [USER]', 'stealth favs to USER'
+    verbose_option
     def_command :user
 
     desc 'id [STATUS ID]', 'stealth favs to STATUS'
+    verbose_option
     def_command :id
   end
 
--- lib/stealth_favs/main.rb
+++ lib/stealth_favs/main.rb
@@ -6,8 +6,12 @@ require 'yaml'
 module StealthFavs
   module Main extend self
 
+    CredentialFile = File.expand_path('~') + '/.credential.yml'
+
     def config_twitter
-      yaml = YAML.load(File.open(Setting).read)
+      return @client if @client
+
+      yaml = YAML.load(File.open(CredentialFile).read)
       @client ||= Twitter::Client.new(
         consumer_key: yaml['consumer_key'],
         consumer_secret: yaml['consumer_secret'],
@@ -16,9 +20,34 @@ module StealthFavs
       )
     end
 
-    def user(opts, name)
-      p opts
-      p name
+    private :config_twitter
+
+    def cmd_id(opts, id, num_times = 20)
+      cl = config_twitter
+      begin
+        (1..num_times).each do |i|
+          cl.favorite id
+          puts "[#{i}] Favorited status #{id}" if opts[:verbose]
+          sleep 0.1
+          cl.unfavorite id
+          puts "[#{i}] Unfavorited status #{id}" if opts[:verbose]
+          sleep 0.1
+        end
+      rescue Twitter::Error::NotFound
+        STDERR.puts "status is not found"
+      end
     end
+
+    def cmd_user(opts, name, num_times = 20)
+      cl = config_twitter
+      begin
+        id = cl.user_timeline(name).first.id
+        puts "A target status is #{id}." if opts[:verbose]
+      rescue Twitter::Error::NotFound
+        STDERR.puts "user is not found"
+      end
+      cmd_id opts, id, num_times
+    end
+
   end  # module Main
 end  # module StealthFavs
